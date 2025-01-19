247ce59bacc78348216af55f676d790ddd1e47c9
Salttt <dolyons2@gmail.com>
2025-01-19 00:27:15 -0500

    completed asteroid game

--- circleshape.py
+++ circleshape.py
@@ -20,3 +20,12 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collision(self, circleshape):
+        distance = self.position.distance_to(circleshape.position)
+        sum_of_radii = self.radius + circleshape.radius
+
+        if distance <= sum_of_radii:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,27 +1,60 @@
 import pygame
+import sys
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
 from player import Player
+from shot import Shot
 from constants import * 
 
 def main():
     pygame.init()
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (updatable, drawable, shots)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     clock = pygame.time.Clock()
     player = Player(SCREEN_HEIGHT/ 2, SCREEN_WIDTH / 2)
+    asteroid_field = AsteroidField()
+
+
 
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        for sprite in updatable:
+            sprite.update(dt)
+        
+        for asteroid in asteroids:
+            if player.collision(asteroid):
+                print("Game Over!")
+                sys.exit()
 
-        player.draw(screen)
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.split()
+                    shot.kill()
+
+        for sprite in drawable:
+            sprite.draw(screen)
 
         pygame.display.flip()
     
-        dt = clock.tick()/1000
-        
+        dt = clock.tick(60)/1000
+
+
+
     
 
 if __name__ == "__main__":
